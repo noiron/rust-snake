63062e969610c1a7883f2015fa11a20e93afc6f2
wu kai <wukai2048@gmail.com>
2023-01-15 23:54:04 +0800

    add food

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,5 @@
 use piston_window::{types::Color, Context, G2d, Key};
+use rand::Rng;
 
 use crate::{drawing::draw_block, snake::Snake};
 
@@ -13,7 +14,10 @@ pub enum Direction {
 }
 
 pub struct Game {
+    width: u32,
+    height: u32,
     snake: Snake,
+    has_food: bool,
     food_x: u32,
     food_y: u32,
     waiting_time: f64,
@@ -23,7 +27,10 @@ pub struct Game {
 impl Game {
     pub fn new() -> Game {
         Game {
+            width: 20,
+            height: 20,
             snake: Snake::new(),
+            has_food: true,
             food_x: 8,
             food_y: 8,
             waiting_time: 0.0,
@@ -49,6 +56,10 @@ impl Game {
     pub fn update(&mut self, delta_time: f64) {
         self.waiting_time += delta_time;
 
+        if !self.has_food {
+            self.add_food();
+        }
+
         if self.waiting_time > 0.2 {
             self.snake.update(self.direction);
             self.eat_food();
@@ -60,7 +71,18 @@ impl Game {
         let (head_x, head_y) = self.snake.head();
         if head_x == self.food_x && head_y == self.food_y {
             self.snake.eat();
+            self.has_food = false;
         }
+    }
+
+    pub fn add_food(&mut self) {
+        let mut rng = rand::thread_rng();
+        let x: u32 = rng.gen_range(0..self.width);
+        let y: u32 = rng.gen_range(0..self.height);
+        // TODO: 需要保证新位置不会和 body 重叠
+        self.food_x = x;
+        self.food_y = y;
+        self.has_food = true;
     }
 
     pub fn key_pressed(&mut self, key: Key) {
